7a2de7734b1417fda83c6798e3352a8eada481ca
ich777 <ich777@noreply.example.org>
2019-06-03 19:40:32 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+
+
+cd ${SERVER_DIR}
+wget -qO ${GAME_VERSION} https://github.com/OpenTTD/OpenTTD/archive/1.9.1.zip
+unzip -d ${SERVER_DIR}/compileopenttd ${GAME_VERSION}
+COMPVDIR="$(find ${SERVER_DIR}/compileopenttd -name Open* -print -quit)"
+cd $COMPVDIR
+$COMPVDIR/configure --prefix-dir=/serverdata/serverfiles --enable-dedicated --personal-dir=/serverfiles/openttd
+CORES_AVAILABLE="$(getconf _NPROCESSORS_ONLN)"
+make --jobs=$CORES_AVAILABLE
+make install
+rm -R ${SERVER_DIR}/compileopenttd
+cd ${SERVER_DIR}/games
+mkdir baseset
+cd ${SERVER_DIR}/games/baseset
+
+
+wget -q ${GFXPACK_URL}
+unzip ${GFXPACK_URL##*/}
+TAR="$( echo "${GFXPACK_URL##*/}" | rev | cut -d "." -f2- | rev)"
+tar -xf $TAR.tar
+rm ${GFXPACK_URL##*/}
+rm $TAR.tar
+
+
 echo "---sleep---"
 sleep infinity
 
@@ -8,7 +33,5 @@ chmod -R 770 ${DATA_DIR}
 echo "---Server ready---"
 
 echo "---Start Server---"
-${SERVER_DIR}/srcds_run -game ${GAME_NAME} ${GAME_PARAMS} -console +port ${GAME_PORT}
-
-
-
+cd ${SERVER_DIR}
+${SERVER_DIR}/games/openttd -D ${GAME_PARAMS}

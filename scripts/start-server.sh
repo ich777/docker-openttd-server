c6c0a10937746d974c26bb8a3173368cb1d6feb1
ich777 <christophhummer@gmail.com>
2021-09-26 15:51:12 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,42 +1,48 @@
 #!/bin/bash
 echo "---Container under construction---"
 sleep infinity
-
 CUR_V="$(find ${SERVER_DIR} -name installed_v_* | cut -d "_" -f3)"
 if [ "${GAME_VERSION}" = "latest" ]; then
-	echo "---Getting latest OpenTTD build version...---"
-	LAT_V="$(curl -s https://cdn.openttd.org/openttd-releases/latest.yaml | grep -B1 "stable" | grep "version:" | cut -d ' ' -f3)"
-	echo "---Latest OpenTTD build version is: $LAT_V---"
-	INSTALL_V=$LAT_V
-	if [ -z $LAT_V ]; then
-		if [ -z $CUR_V ]; then
-			echo "---Something went wrong, couldn't get latest build version---"
-			sleep infinity
-		else
-			echo "---Can't get latest OpenTTD build version, falling back to installed version $CUR_V!---"
-			INSTALL_V=$CUR_V
-		fi
-	fi
-else
-	INSTALL_V=${GAME_VERSION}
+  echo "---Trying to get latest stable OpenTTD version...---"
+  LAT_V="$(curl -s https://cdn.openttd.org/openttd-releases/latest.yaml | grep -B1 "stable" | grep "version:" | cut -d ' ' -f3)"
+  if [ ! -z ${LAT_V} ]; then
+    echo "---Latest stable OpenTTD version is: $LAT_V---"
+  fi
+elif [ "${GAME_VERSION}" = "testing" ]; then
+  echo "---Trying to get latest testing OpenTTD version...---"
+  LAT_V="$(curl -s https://cdn.openttd.org/openttd-releases/latest.yaml | grep -B1 "testing" | grep "version:" | cut -d ' ' -f3)"
+  if [ ! -z ${LAT_V} ]; then
+    echo "---Latest testing OpenTTD version is: $LAT_V---"
+  fi
 fi
 
-GFX_PK_CUR_V="$(cat ${SERVER_DIR}/games/baseset/changelog.txt 2>/dev/null | head -1 | cut -d ' ' -f2)"
+if [ -z $LAT_V ]; then
+  if [ -z $CUR_V ]; then
+    echo "---Something went wrong, couldn't get latest version---"
+    sleep infinity
+  else
+    echo "---Can't get latest OpenTTD build version, falling back to installed version $CUR_V!---"
+    LAT_V=$CUR_V
+  fi
+fi
+
+GFX_PK_CUR_V="$(cat ${SERVER_DIR}/baseset/changelog.txt 2>/dev/null | head -1 | cut -d ' ' -f2)"
 if [ "${GFX_PK_V}" = "latest" ]; then
-	echo "---Getting latest OpenGFX version...---"
-	GFX_PK_V="$(curl -s https://cdn.openttd.org/opengfx-releases/latest.yaml | grep "version:" | cut -d ' ' -f3)"
-	echo "---Latest OpenGFX version is: $GFX_PK_V---"
-	if [ -z ${GFX_PK_V} ]; then
-		if [ -z $GFX_PK_CUR_V ]; then
-			echo "---Something went wrong, couldn't get latest build version---"
-			sleep infinity
-		else
-			echo "---Can't get latest OpenGFX version, falling back to installed version $GFX_PK_CUR_V!---"
-			GFX_PK_V=$GFX_PK_CUR_V
-	fi
-	fi
+  echo "---Getting latest OpenGFX version...---"
+  GFX_PK_V="$(curl -s https://cdn.openttd.org/opengfx-releases/latest.yaml | grep "version:" | cut -d ' ' -f3)"
+  if [ -z ${GFX_PK_V} ]; then
+    if [ -z $GFX_PK_CUR_V ]; then
+      echo "---Something went wrong, couldn't get latest build version---"
+      sleep infinity
+    else
+      echo "---Can't get latest OpenGFX version, falling back to installed version $GFX_PK_CUR_V!---"
+      GFX_PK_V=$GFX_PK_CUR_V
+    fi
+  else
+    echo "---Latest OpenGFX version is: $GFX_PK_V---"
+  fi
 else
-	echo "---Manually set OpenGFX version to ${GFX_PK_V}---"
+  echo "---Manually set OpenGFX version to ${GFX_PK_V}---"
 fi
 
 if [ ! -s ${SERVER_DIR}/installed_v_$LAT_V ]; then
@@ -44,126 +50,64 @@ if [ ! -s ${SERVER_DIR}/installed_v_$LAT_V ]; then
 fi
 
 echo "---Version Check---"
-if [ ! -f ${SERVER_DIR}/games/openttd ]; then
-	echo
-	echo "-------------------------------------"
-	echo "---OpenTTD not found! Downloading,---"
-	echo "---compiling and installing v$INSTALL_V---"
-	echo "---Please be patient, this can take--"
-	echo "---some time, waiting 15 seconds..---"
-	echo "-------------------------------------"
-	sleep 15
-	cd ${SERVER_DIR}
-	if wget -q -nc --show-progress --progress=bar:force:noscroll -O installed_v_$INSTALL_V https://cdn.openttd.org/openttd-releases/$INSTALL_V/openttd-$INSTALL_V-linux-generic-amd64.tar.xz ; then
-		echo "---Successfully downloaded OpenTTD v$INSTALL_V---"
-	else
-		echo "---Can't download OpenTTD v$INSTALL_V putting server into sleep mode---"
-		sleep infinity
-	fi
-	mkdir compileopenttd
-	tar -xf installed_v_$INSTALL_V -C ${SERVER_DIR}/compileopenttd/
-	COMPVDIR="$(find ${SERVER_DIR}/compileopenttd -name open* -print -quit)"
-	mkdir $COMPVDIR/build
-	cd $COMPVDIR/build
-	cmake -DCMAKE_INSTALL_PREFIX:PATH=${SERVER_DIR} ..
-	if [ ! -z "${COMPILE_CORES}" ]; then
-		CORES_AVAILABLE=${COMPILE_CORES}
-	else
-		CORES_AVAILABLE="$(getconf _NPROCESSORS_ONLN)"
-	fi
-	make --jobs=$CORES_AVAILABLE
-	make install
-	rm -R ${SERVER_DIR}/compileopenttd
-	if [ ! -f ${SERVER_DIR}/games/openttd ]; then 
-		echo "---Something went wrong, couldn't install OpenTTD v$INSTALL_V---"
-		sleep infinity
-	else
-		echo "---OpenTTD v$INSTALL_V installed---"
-	fi
+if [ ! -f ${SERVER_DIR}/openttd ]; then
+  echo
+  echo "-------------------------------------"
+  echo "---OpenTTD not found! Downloading,---"
+  echo "-----and installing v$LAT_V-----"
+  echo "-------------------------------------"
+  cd ${SERVER_DIR}
+  if wget -q -nc --show-progress --progress=bar:force:noscroll -O ${SERVER_DIR}/installed_v_${LAT_V} https://cdn.openttd.org/openttd-releases/${LAT_V}/openttd-${LAT_V}-linux-generic-amd64.tar.xz ; then
+    echo "---Successfully downloaded OpenTTD v$LAT_V---"
+  else
+    echo "---Can't download OpenTTD v$LAT_V putting server into sleep mode---"
+    sleep infinity
+  fi
+  tar --strip-components=1 -xf installed_v_$LAT_V -C ${SERVER_DIR}/
 elif [ "$INSTALL_V" != "$CUR_V" ]; then
-	echo
-	echo "-------------------------------------------------"
-	echo "---Version missmatch, installing v$INSTALL_V----------"
-	echo "------Changing from v$CUR_V to v$INSTALL_V-------------"
-	echo "----Please be patient this can take some time----"
-	echo "---------------Waiting 15 seconds----------------"
-	echo "-------------------------------------------------"
-	echo
-	sleep 15
-	cd ${SERVER_DIR}
-	rm installed_v_$CUR_V
-	rm -R games
-	rm -R share
-	if wget -q -nc --show-progress --progress=bar:force:noscroll -O installed_v_$INSTALL_V https://cdn.openttd.org/openttd-releases/$INSTALL_V/openttd-$INSTALL_V-linux-generic-amd64.tar.xz ; then
-		echo "---Successfully downloaded OpenTTD v$INSTALL_V---"
-	else
-		echo "---Can't download OpenTTD v$INSTALL_V putting server into sleep mode---"
-		sleep infinity
-	fi
-	mkdir compileopenttd
-	tar -xf installed_v_$INSTALL_V -C ${SERVER_DIR}/compileopenttd/
-	COMPVDIR="$(find ${SERVER_DIR}/compileopenttd -name openttd-* -print -quit)"
-	mkdir $COMPVDIR/build
-	cd $COMPVDIR/build
-	cmake -DCMAKE_INSTALL_PREFIX:PATH=${SERVER_DIR} ..
-	if [ ! -z "${COMPILE_CORES}" ]; then
-		CORES_AVAILABLE=${COMPILE_CORES}
-	else
-		CORES_AVAILABLE="$(getconf _NPROCESSORS_ONLN)"
-	fi
-	make --jobs=$CORES_AVAILABLE
-	make install
-	rm -R ${SERVER_DIR}/compileopenttd
-	if [ ! -f ${SERVER_DIR}/games/openttd ]; then 
-		echo "---Something went wrong, couldn't install OpenTTD v$INSTALL_V---"
-		sleep infinity
-	else
-		echo "---OpenTTD v$INSTALL_V installed---"
-	fi
+  echo
+  echo "-------------------------------------------------"
+  echo "-----Version missmatch, installing v$LAT_V-----"
+  echo "-------------------------------------------------"
+  echo
+  cd ${SERVER_DIR}
+  if wget -q -nc --show-progress --progress=bar:force:noscroll -O ${SERVER_DIR}/installed_v_${LAT_V} https://cdn.openttd.org/openttd-releases/${LAT_V}/openttd-${LAT_V}-linux-generic-amd64.tar.xz ; then
+    echo "---Successfully downloaded OpenTTD v$LAT_V---"
+  else
+    echo "---Can't download OpenTTD v$LAT_V putting server into sleep mode---"
+    sleep infinity
+  fi
+  tar --strip-components=1 -xf installed_v_$LAT_V -C ${SERVER_DIR}/
 else
 	echo "---OpenTTD v$LAT_V found---"
 fi
 
-if [ ! -d ${SERVER_DIR}/games/baseset ]; then
-	echo "---OpenGFX not found, downloading...---"
-	cd ${SERVER_DIR}/games
-	mkdir ${SERVER_DIR}/games/baseset
-	cd ${SERVER_DIR}/games/baseset
-	if wget -q -nc --show-progress --progress=bar:force:noscroll -O opengfx-${GFX_PK_V}.zip https://cdn.openttd.org/opengfx-releases/${GFX_PK_V}/opengfx-${GFX_PK_V}-all.zip ; then
-		echo "---Successfully downloaded OpenGFX---"
-	else
-		echo "---Can't download OpenGFX putting server into sleep mode---"
-		sleep infinity
-	fi
-	unzip opengfx-${GFX_PK_V}.zip
-	tar --strip-components=1 -xf opengfx-${GFX_PK_V}.tar
-	rm opengfx-${GFX_PK_V}.zip opengfx-${GFX_PK_V}.tar
-	GFX="$(find ${SERVER_DIR}/games/baseset -maxdepth 1 -name '*grf')"
-	if [ -z "$GFX" ]; then
-		echo "---Something went wrong, couldn't install OpenGFX---"
-		sleep infinity
-	fi
+if [ ! -f ${SERVER_DIR}/baseset/changelog.txt ]; then
+  echo "---OpenGFX not found, downloading...---"
+  cd ${SERVER_DIR}
+  if wget -q -nc --show-progress --progress=bar:force:noscroll -O ${SERVER_DIR}/opengfx-${GFX_PK_V}.zip https://cdn.openttd.org/opengfx-releases/${GFX_PK_V}/opengfx-${GFX_PK_V}-all.zip ; then
+    echo "---Successfully downloaded OpenGFX---"
+  else
+    echo "---Can't download OpenGFX putting server into sleep mode---"
+    sleep infinity
+  fi
+  unzip ${SERVER_DIR}/opengfx-${GFX_PK_V}.zip
+  tar --strip-components=1 -xf ${SERVER_DIR}/opengfx-${GFX_PK_V}.tar -C ${SERVER_DIR}/baseset/
+  rm ${SERVER_DIR}/opengfx-${GFX_PK_V}.zip ${SERVER_DIR}/opengfx-${GFX_PK_V}.tar
 elif [ "$GFX_PK_CUR_V" != "$GFX_PK_V" ]; then
-	echo "---Newer version for OpenGFX found, installing!---"
-	rm -R ${SERVER_DIR}/games/baseset
-	mkdir ${SERVER_DIR}/games/baseset
-	cd ${SERVER_DIR}/games/baseset
-	if wget -q -nc --show-progress --progress=bar:force:noscroll -O opengfx-${GFX_PK_V}.zip https://cdn.openttd.org/opengfx-releases/${GFX_PK_V}/opengfx-${GFX_PK_V}-all.zip ; then
-		echo "---Successfully downloaded OpenGFX---"
-	else
-		echo "---Can't download OpenGFX putting server into sleep mode---"
-		sleep infinity
-	fi
-	unzip opengfx-${GFX_PK_V}.zip
-	tar --strip-components=1 -xf opengfx-${GFX_PK_V}.tar
-	rm opengfx-${GFX_PK_V}.zip opengfx-${GFX_PK_V}.tar
-	GFX="$(find ${SERVER_DIR}/games/baseset -maxdepth 1 -name '*grf')"
-	if [ -z "$GFX" ]; then
-		echo "---Something went wrong, couldn't install OpenGFX---"
-		sleep infinity
-	fi
+  echo "---Newer version for OpenGFX found, installing!---"
+  cd ${SERVER_DIR}
+  if wget -q -nc --show-progress --progress=bar:force:noscroll -O ${SERVER_DIR}/opengfx-${GFX_PK_V}.zip https://cdn.openttd.org/opengfx-releases/${GFX_PK_V}/opengfx-${GFX_PK_V}-all.zip ; then
+    echo "---Successfully downloaded OpenGFX---"
+  else
+    echo "---Can't download OpenGFX putting server into sleep mode---"
+    sleep infinity
+  fi
+  unzip ${SERVER_DIR}/opengfx-${GFX_PK_V}.zip
+  tar --strip-components=1 -xf ${SERVER_DIR}/opengfx-${GFX_PK_V}.tar -C ${SERVER_DIR}/baseset/
+  rm ${SERVER_DIR}/opengfx-${GFX_PK_V}.zip ${SERVER_DIR}/opengfx-${GFX_PK_V}.tar
 else
-	echo "---OpenGFX found---"
+  echo "---OpenGFX found---"
 fi
 
 echo "---Prepare Server---"
@@ -178,7 +122,7 @@ echo "---Server ready---"
 
 echo "---Start Server---"
 cd ${SERVER_DIR}
-screen -S OpenTTD -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m ${SERVER_DIR}/games/openttd -D ${GAME_PARAMS}
+screen -S OpenTTD -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m ${SERVER_DIR}/openttd -D ${GAME_PARAMS}
 sleep 2
 if [ "${ENABLE_WEBCONSOLE}" == "true" ]; then
     /opt/scripts/start-gotty.sh 2>/dev/null &
